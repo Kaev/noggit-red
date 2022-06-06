47d4779f0e0dbdfa838d6d8f22eeaf8a22d45e1e
Alister <alister.greg@gmail.com>
2022-06-06 22:49:35 +0100

    adding in loadingScreen repository

--- src/noggit/database/repositories/loadingScreen/_seed.h
+++ src/noggit/database/repositories/loadingScreen/_seed.h
@@ -5,6 +5,14 @@
 
 namespace Noggit::Database::Repositories
 {
+	struct LoadingScreenEntry
+	{
+		int Id;
+		std::string Name;
+		std::string FileName;
+		bool HasWideScreen;
+	};
+
 	class ILoadingScreenRepository
 	{
 
@@ -17,6 +25,26 @@ namespace Noggit::Database::Repositories
 		WotlkLoadingScreenRepository(const std::filesystem::path& database_path) : _databasePath(database_path)
 		{
 
+		}
+
+		std::vector<LoadingScreenEntry> GetLoadingScreens()
+		{
+			SQLite::Database   db(_databasePath.generic_string());
+			SQLite::Statement  query(db, "SELECT ID, Name, FileName, HasWideScreen FROM LoadingScreens;");
+
+			auto loadingScreens = std::vector<LoadingScreenEntry>();
+			while (query.executeStep())
+			{
+				auto loadingScreen = LoadingScreenEntry();
+				loadingScreen.Id = query.getColumn(0).getInt();
+				loadingScreen.Name = query.getColumn(1).getString();
+				loadingScreen.FileName = query.getColumn(2).getString();
+				loadingScreen.HasWideScreen = static_cast<bool>(query.getColumn(3).getInt());
+
+				loadingScreens.push_back(loadingScreen);
+			}
+
+			return loadingScreens;
 		}
 	};
 
