34a8839bed5fbf0a3a2e6f18897b33ce029504e6
Alister <alister.greg@gmail.com>
2022-07-17 18:17:48 +0100

    Adding GetAllAreas Lookup

--- src/noggit/database/repositories/areaTable/_seed.h
+++ src/noggit/database/repositories/areaTable/_seed.h
@@ -34,6 +34,7 @@ namespace Noggit::Database::Repositories
 		virtual std::vector<AreaTableEntry> GetAllAreasForMapId(unsigned int areaId) = 0;
 		virtual uint32_t GetParentAreaId(unsigned int areaId) = 0;
 		virtual LocaleString GetAreaName(unsigned int areaId) = 0;
+		virtual std::vector<AreaTableEntry> GetAllAreas() = 0;
 	};
 
 	class WotlkAreaTableRepository : public IAreaTableRepository, public LocaleStringRepository
@@ -42,6 +43,46 @@ namespace Noggit::Database::Repositories
 	public:
 		WotlkAreaTableRepository(const std::filesystem::path& database_path) : LocaleStringRepository(database_path) ,_databasePath(database_path)
 		{
+		}
+
+		std::vector<AreaTableEntry> GetAllAreas() override
+		{
+			SQLite::Database   db(_databasePath.generic_string());
+			SQLite::Statement  query(db, "SELECT ID, ContinentID, ParentAreaID, AreaBit, Flags, SoundProviderPref, SoundProviderPrefUnderwater, AmbienceID, ZoneMusic, IntroSound, "
+				"ExplorationLevel, AreaName_lang, FactionGroupMask, LiquidTypeID_0, LiquidTypeID_1, LiquidTypeID_2, LiquidTypeID_3, MinElevation, Ambient_multiplier, LightID "
+				"FROM AreaTable;");
+
+			auto areas = std::vector<AreaTableEntry>();
+			while (query.executeStep())
+			{
+				auto area = AreaTableEntry();
+				area.Id = query.getColumn(0).getInt();
+				area.ContinentId = query.getColumn(1).getInt();
+				area.ParentAreaId = query.getColumn(2).getInt();
+				area.AreaBit = query.getColumn(3).getInt();
+				area.Flags = query.getColumn(4).getInt();
+				area.SoundProviderPref = query.getColumn(5).getInt();
+				area.SoundProviderPrefUnderwater = query.getColumn(6).getInt();
+				area.AmbienceID = query.getColumn(7).getInt();
+				area.ZoneMusic = query.getColumn(8).getInt();
+				area.IntroSound = query.getColumn(9).getInt();
+				area.ExplorationLevel = query.getColumn(10).getInt();
+				area.AreaName = GetLocaleString(query.getColumn(11).getInt());
+				area.FactionGroupMask = query.getColumn(12).getInt();
+
+				for (auto liquidType = 13; liquidType < 17; liquidType++)
+				{
+					area.LiquidTypeID[liquidType - 13] = query.getColumn(13).getInt();
+				}
+
+				area.MinElevation = query.getColumn(17).getDouble();
+				area.Ambient_multiplier = query.getColumn(18).getDouble();
+				area.LightID = query.getColumn(19).getInt();
+
+				areas.push_back(area);
+			}
+
+			return areas;
 		}
 
 		std::vector<AreaTableEntry> GetAllAreasForMapId(unsigned int areaId) override
